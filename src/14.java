687974d913c92687c9c867515712180fcdb9f7e2
cloughertyancell806 <cloughertyancell806@gmail.com>
2025-03-19 05:45:37 +0000

    Add 14.java

---
+++ src/14.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 5;
+        System.out.println("The sum of " + x + " and " + y + " is " + (x + y));
+    }
+}
