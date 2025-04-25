2575eeb7fbedaea52d8e185adff2ffb455d250f5
cloughertyancell806 <cloughertyancell806@gmail.com>
2025-04-25 02:45:00 +0000

    Add 33.java

---
+++ src/33.java
@@ -0,0 +1,18 @@
+public class CodingChallenge {
+    public static void main(String[] args) {
+        // Example of using a switch statement to change the output based on input
+        int number = 5;
+        
+        switch(number) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            default:
+                System.out.println("The value is not one or two.");
+                break;
+        }
+    }
+}
